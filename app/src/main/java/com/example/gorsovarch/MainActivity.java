795a6d676a46336e36aefa00a601537e70e6d163
Seva Archakov <seva.archakov@gmail.com>
2019-07-10 20:52:12 +0300

    First commit

---
+++ app/src/main/java/com/example/gorsovarch/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.gorsovarch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    MainActivity a;
+    EditText editLogin, editPassword;
+    Button enter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editLogin = (EditText)findViewById(R.id.editTextLogin);
+        editPassword = (EditText)findViewById(R.id.editTextPassword);
+        enter = (Button)findViewById(R.id.btnEnter);
+        enter.setTextColor(getResources().getColor(R.color.DimGrey));
+        editLogin.addTextChangedListener(loginTextWatcher);
+        editPassword.addTextChangedListener(loginTextWatcher);
+        getSupportActionBar().hide();
+    }
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String login = editLogin.getText().toString().trim();
+            String password = editPassword.getText().toString().trim();
+            if(login.isEmpty() || password.isEmpty())
+                enter.setTextColor(getResources().getColor(R.color.DimGrey));
+            else enter.setTextColor(getResources().getColor(R.color.White));
+            enter.setEnabled(!login.isEmpty() && !password.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String login = editLogin.getText().toString().trim();
+            String password = editPassword.getText().toString().trim();
+            if(login.isEmpty() || password.isEmpty())
+                enter.setTextColor(getResources().getColor(R.color.DimGrey));
+            else enter.setTextColor(getResources().getColor(R.color.White));
+            enter.setEnabled(!login.isEmpty() && !password.isEmpty());
+        }
+    };
+}
