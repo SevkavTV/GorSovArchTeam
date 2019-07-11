019c3b326691cc1342db5dc7861255644b0dc564
Seva Archakov <seva.archakov@gmail.com>
2019-07-11 23:00:46 +0300

    Adobe and FileOpener added

--- app/src/main/java/com/example/gorsovarch/HomeActivity.java
+++ app/src/main/java/com/example/gorsovarch/HomeActivity.java
@@ -2,9 +2,14 @@ package com.example.gorsovarch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Environment;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +17,10 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -20,7 +28,7 @@ import java.util.List;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     List<RecentAppView> recentApps;
-    ImageView doc;
+    ImageView doc, adobe, chrome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +37,11 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         Runnable runnable = new CountDownRunner();
         Thread currTime = null;
         doc = (ImageView) findViewById(R.id.documents);
+        adobe = (ImageView) findViewById(R.id.adobe);
+        chrome = (ImageView) findViewById(R.id.chrome);
         doc.setOnClickListener(this);
+        adobe.setOnClickListener(this);
+        chrome.setOnClickListener(this);
         currTime = new Thread(runnable);
         currTime.start();
         recentApps = new ArrayList<>();
@@ -58,6 +70,21 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.documents:
                 i = new Intent(this, DocumentsActivity.class);
                 startActivity(i);
+                break;
+            case R.id.adobe:
+                try {
+                    Context ctx = this;
+                    i = ctx.getPackageManager().getLaunchIntentForPackage("com.adobe.reader");
+                    ctx.startActivity(i);
+                }catch(Exception e){
+                    i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("market://details?id=com.adobe.reader"));
+                    startActivity(i);
+                }
+                break;
+            case R.id.chrome:
+                File file = new File("/storage/emulated/0/Download/lec8.pdf");
+                openFile(file);
                 break;
         }
     }
@@ -115,5 +142,56 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             ((ImageView) currView.findViewById(R.id.image)).setImageResource(R.drawable.i1);
         }
         return currView;
+    }
+    private void openFile(File url) {
+        Context context = this;
+        try {
+
+            Uri uri = Uri.fromFile(url);
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            if (url.toString().contains(".doc") || url.toString().contains(".docx")) {
+                // Word document
+                intent.setDataAndType(uri, "application/msword");
+            } else if (url.toString().contains(".pdf")) {
+                // PDF file
+                intent.setDataAndType(uri, "application/pdf");
+            } else if (url.toString().contains(".ppt") || url.toString().contains(".pptx")) {
+                // Powerpoint file
+                intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+            } else if (url.toString().contains(".xls") || url.toString().contains(".xlsx")) {
+                // Excel file
+                intent.setDataAndType(uri, "application/vnd.ms-excel");
+            } else if (url.toString().contains(".zip") || url.toString().contains(".rar")) {
+                // WAV audio file
+                intent.setDataAndType(uri, "application/x-wav");
+            } else if (url.toString().contains(".rtf")) {
+                // RTF file
+                intent.setDataAndType(uri, "application/rtf");
+            } else if (url.toString().contains(".wav") || url.toString().contains(".mp3")) {
+                // WAV audio file
+                intent.setDataAndType(uri, "audio/x-wav");
+            } else if (url.toString().contains(".gif")) {
+                // GIF file
+                intent.setDataAndType(uri, "image/gif");
+            } else if (url.toString().contains(".jpg") || url.toString().contains(".jpeg") || url.toString().contains(".png")) {
+                // JPG file
+                intent.setDataAndType(uri, "image/jpeg");
+            } else if (url.toString().contains(".txt")) {
+                // Text file
+                intent.setDataAndType(uri, "text/plain");
+            } else if (url.toString().contains(".3gp") || url.toString().contains(".mpg") ||
+                    url.toString().contains(".mpeg") || url.toString().contains(".mpe") || url.toString().contains(".mp4") || url.toString().contains(".avi")) {
+                // Video files
+                intent.setDataAndType(uri, "video/*");
+            } else {
+                intent.setDataAndType(uri, "/*");
+            }
+
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(context, "No application found which can open the file", Toast.LENGTH_SHORT).show();
+        }
     }
 }
